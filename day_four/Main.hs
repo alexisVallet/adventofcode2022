b86a4959995bb5889d7564659c94b54f4784efdf
Alexis Vallet <alexis.vallet@gmail.com>
2022-12-04 14:38:52 +0900

    Added missing extension. Typo fix.

--- day_four/Main.hs
+++ day_four/Main.hs
@@ -29,11 +29,11 @@ contains (s1, e1) (s2, e2) = s1 <= s2 && e1 >= e2
 symContains :: Range -> Range -> Bool
 symContains r1 r2 = contains r1 r2 || contains r2 r1
 
-overlap :: Range -> Range -> Bool
-overlap (s1, e1) (s2, e2) = not $ e1 < s2 || e2 < s1
+overlaps :: Range -> Range -> Bool
+overlaps (s1, e1) (s2, e2) = not $ e1 < s2 || e2 < s1
 
 main :: IO ()
 main = do
   sectionAssignments <- parseOrDie sectionAssignmentParser <$> TIO.readFile "input_4"
   putStrLn $ "Question 1 answer is: " ++ show (sum $ fmap (fromEnum . uncurry symContains) sectionAssignments)
-  putStrLn $ "Question 2 answer is: " ++ show (sum $ fmap (fromEnum . uncurry overlap) sectionAssignments)
+  putStrLn $ "Question 2 answer is: " ++ show (sum $ fmap (fromEnum . uncurry overlaps) sectionAssignments)
