548ab67fac4d0e4bfd14612048e27c3271cbce3c
Alexis Vallet <alexis.vallet@gmail.com>
2022-12-04 14:30:42 +0900

    formatting

--- lib/ParseUtils.hs
+++ lib/ParseUtils.hs
@@ -1,11 +1,10 @@
 module ParseUtils (parseOrDie) where
 
+import Data.Text (Text)
 import Data.Void
 import Text.Megaparsec
-import Data.Text (Text)
-
 
 parseOrDie :: Parsec Void Text a -> Text -> a
 parseOrDie parser contents = case runParser parser "" contents of
-    Left err -> error $ errorBundlePretty err
-    Right out -> out
+  Left err -> error $ errorBundlePretty err
+  Right out -> out
