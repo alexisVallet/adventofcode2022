5a51554e6b4146a5730e9aa3fc4bac5f50d6bf14
Alexis Vallet <alexis.vallet@gmail.com>
2022-12-06 17:08:19 +0900

    Added missing files.

---
+++ day6/Main.hs
@@ -0,0 +1,27 @@
+module Main where
+
+import Control.Exception
+import Data.List
+import Data.Set qualified as Set
+import Data.List.Split (divvy)
+
+
+findFirstUniqueSubseqIndex :: (Ord a) => Int -> [a] -> Maybe Int
+findFirstUniqueSubseqIndex chunkSize as =
+    fmap ((+ chunkSize) . fst . head)
+    $ find (\chunk -> length (Set.fromList (snd <$> chunk)) == chunkSize) 
+    $ divvy chunkSize 1
+    $ zip [0..] as
+
+main :: IO ()
+main = do
+    let testActualQ1 = findFirstUniqueSubseqIndex 4 "bvwbjplbgvbhsrlpgdmjqwftvncz"
+        testActualQ2 = findFirstUniqueSubseqIndex 14 "mjqjpqmgbljsphdztnvjfqwrcgsmlb"
+    assert (testActualQ1 == Just 5 && testActualQ2 == Just 19) $ do
+        fileContents <- readFile "input_6"
+        putStrLn 
+            $ "Question 1 answer is: " 
+            ++ show (findFirstUniqueSubseqIndex 4 fileContents)
+        putStrLn
+            $ "Question 2 answer is: "
+            ++ show (findFirstUniqueSubseqIndex 14 fileContents)
