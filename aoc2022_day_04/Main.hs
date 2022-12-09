b08d04a3335d95819fedf2f11c71a940440b3aff
Alexis Vallet <alexis.vallet@gmail.com>
2022-12-09 20:11:17 +0900

    Day 7 solution

---
+++ aoc2022_day_04/Main.hs
@@ -0,0 +1,39 @@
+module Main where
+
+import Data.Text (Text)
+import Data.Text.IO qualified as TIO
+import Data.Void
+import ParseUtils
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import Text.Megaparsec.Char.Lexer
+
+type Range = (Integer, Integer)
+
+sectionAssignmentParser :: Parsec Void Text [(Range, Range)]
+sectionAssignmentParser = many $ do
+  let rangeParser = do
+        start <- decimal
+        char '-'
+        end <- decimal
+        return (start, end)
+  range1 <- rangeParser
+  char ','
+  range2 <- rangeParser
+  newline
+  return (range1, range2)
+
+contains :: Range -> Range -> Bool
+contains (s1, e1) (s2, e2) = s1 <= s2 && e1 >= e2
+
+symContains :: Range -> Range -> Bool
+symContains r1 r2 = contains r1 r2 || contains r2 r1
+
+overlaps :: Range -> Range -> Bool
+overlaps (s1, e1) (s2, e2) = not $ e1 < s2 || e2 < s1
+
+main :: IO ()
+main = do
+  sectionAssignments <- parseOrDie sectionAssignmentParser <$> TIO.readFile "input_4"
+  putStrLn $ "Question 1 answer is: " ++ show (sum $ fmap (fromEnum . uncurry symContains) sectionAssignments)
+  putStrLn $ "Question 2 answer is: " ++ show (sum $ fmap (fromEnum . uncurry overlaps) sectionAssignments)
---
+++ aoc2022_day_07/Main.hs
@@ -0,0 +1,138 @@
+{-# LANGUAGE LambdaCase #-}
+module Main where
+
+import Control.Exception
+import Data.Void
+import Data.Either
+import Data.Maybe
+import Data.Map (Map)
+import Data.Map qualified as Map
+import Data.List
+import GHC.Generics
+import Data.Generics.Product.Fields (HasField)
+import Control.Lens hiding (uncons)
+import Data.Generics.Labels ()
+import Data.Text (Text)
+import Data.Text.IO qualified as TIO
+import Text.Megaparsec hiding (State)
+import Text.Megaparsec.Char
+import Text.Megaparsec.Char.Lexer
+import Text.Pretty.Simple (pPrint)
+import Control.Monad.State.Strict
+import Debug.Trace
+import ParseUtils
+
+type Directory = Map String DirOrFile
+
+data DirOrFile = D Directory | F File
+    deriving (Generic, Show)
+
+dirOrFileToEither :: DirOrFile -> Either Directory File
+dirOrFileToEither (D dir) = Left dir
+dirOrFileToEither (F file) = Right file
+
+_D :: Prism' DirOrFile Directory
+_D = prism' D (\d -> case d of
+        D dir -> Just dir
+        F _ -> Nothing)
+
+_F :: Prism' DirOrFile File
+_F = prism' F (\d -> case d of
+    D _ -> Nothing
+    F f -> Just f)
+
+newtype File = File {
+    size :: Int
+} deriving (Generic, Show)
+
+data ParserState = ParserState {
+    curDir :: [String],
+    curFS :: Directory
+} deriving (Generic, Show)
+
+type FSParser = ParsecT Void Text (State ParserState)
+
+initParserState :: ParserState
+initParserState = ParserState {
+    curDir = [],
+    curFS = Map.singleton "/" (D mempty)
+}
+
+dirLens :: [String] -> Traversal' ParserState Directory
+dirLens = foldr accLens #curFS
+    where
+        accLens fname curLens = curLens . at fname . _Just . _D
+
+parseCd :: FSParser ()
+parseCd = do
+    _ <- string "$ cd "
+    dirNameOrParent <- do
+        Right <$> (string ".." >> void newline) <|> Left <$> manyTill anySingle newline
+    case dirNameOrParent of
+        Left dirname -> do
+            #curDir %= (dirname :)
+        Right () -> #curDir %= snd . fromJust . uncons
+
+addDirOrFile :: String -> DirOrFile -> FSParser ()
+addDirOrFile dirname dirOrFile = do
+    curDirVal <- use #curDir
+    dirLens curDirVal . at dirname ?= dirOrFile
+
+parseDir :: FSParser ()
+parseDir = do
+    void $ string "dir "
+    dirname <- manyTill anySingle newline
+    addDirOrFile dirname $ D mempty
+
+parseFile :: FSParser ()
+parseFile = do
+    size <- decimal
+    hspace
+    fname <- manyTill anySingle newline
+    addDirOrFile fname $ F $ File size
+
+parseLs :: FSParser ()
+parseLs = do
+    _ <- string "$ ls\n"
+    void $ some $ do
+        parseDir <|> parseFile
+
+fsParser :: FSParser Directory
+fsParser = do
+    _ <- some $ do
+        parseCd <|> parseLs
+    use #curFS
+
+parseFsOrDie :: Text -> Directory
+parseFsOrDie input =
+    case evalState (runParserT fsParser "" input) initParserState of
+        Left err -> error $ errorBundlePretty err
+        Right dir -> dir
+
+dirSizes :: Directory -> [Int]
+dirSizes dir =
+    let (childDirs, files) = partitionEithers $ dirOrFileToEither <$> Map.elems dir
+        childDirSizes = dirSizes <$> childDirs
+    in sum (size <$> files) + sum (head <$> childDirSizes) : concat childDirSizes
+
+sumOfDirsSmallerThan :: Int -> Directory -> Int
+sumOfDirsSmallerThan thresh dir =
+    let sortedSizes = sort $ dirSizes dir
+    in sum $ takeWhile (<thresh) sortedSizes
+
+sizeOfDirToDelete :: Int -> Directory -> Int
+sizeOfDirToDelete necessaryAmount dir =
+    let sizes = dirSizes dir
+        unused = 70000000 - head sizes
+        (_, large) = partition (< necessaryAmount - unused) sizes
+    in minimum large
+
+main :: IO ()
+main = do
+    testDir <- parseFsOrDie <$> TIO.readFile "aoc22_day07_input_test"
+    let actualSumOfDirs = sumOfDirsSmallerThan 100000 testDir
+        actualSizeToDelete = sizeOfDirToDelete 30000000 testDir
+    assert (actualSumOfDirs == 95437 && actualSizeToDelete == 24933642) $ do
+        dir <- parseFsOrDie <$> TIO.readFile "aoc22_day07_input"
+        putStrLn $ "Question 1 answer is " ++ show (sumOfDirsSmallerThan 100000 dir)
+        putStrLn $ "Question 2 answer is " ++ show (sizeOfDirToDelete 30000000 dir)
