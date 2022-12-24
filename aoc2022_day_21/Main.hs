a80be819b55775a1a64ff614ff1656fd66fb6622
Alexis Vallet <alexisvallet@preferred.jp>
2022-12-24 23:25:19 +0900

    Day 21 solution

--- aoc2022_day_21/Main.hs
+++ aoc2022_day_21/Main.hs
@@ -5,25 +5,44 @@ import Data.Text.IO qualified as TIO
 import Data.Array.Accelerate (Exp, Acc, Scalar, Vector, Array, DIM1)
 import Data.Array.Accelerate qualified as A
 import Data.Array.Accelerate.Interpreter qualified as AI
+import Z3.Monad
+import Z3.Monad qualified as Z3
 import Imports hiding (Vertex)
 import ParseUtils
 
 type Vertex = String
 
-data Node = Constant Int | BinOp (Exp Int -> Exp Int -> Exp Int) Vertex Vertex
+data Op = Add | Sub | Mul | Div
+    deriving (Eq, Ord)
+
+opToExp :: Op -> (Exp Int -> Exp Int -> Exp Int)
+opToExp Add = (+)
+opToExp Sub = (-)
+opToExp Mul = (*)
+opToExp Div = div
+
+opToZ3 :: MonadZ3 z3 => Op -> AST -> AST -> z3 AST
+opToZ3 Add x y = mkAdd [x, y]
+opToZ3 Sub x y = mkSub [x, y]
+opToZ3 Mul x y = mkMul [x, y]
+opToZ3 Div x y = mkDiv x y
+
+data Node = 
+    Constant Int 
+    | BinOp Op Vertex Vertex
 
 type ComputeGraph = Map Vertex Node
 
 parseName = replicateM 4 anySingle
 
-parseOp :: Parsec Void Text (Exp Int -> Exp Int -> Exp Int)
+parseOp :: Parsec Void Text Op
 parseOp = do
     opChar <- oneOf ("+-*/" :: [Char])
     return $ case opChar of
-        '+' -> (+)
-        '-' -> (-)
-        '*' -> (*)
-        '/' -> div
+        '+' -> Add
+        '-' -> Sub
+        '*' -> Mul
+        '/' -> Div
         _ -> error "This should never happen!"
 
 parseNode :: Parsec Void Text Node
@@ -47,7 +66,7 @@ computeNodeExp name graph =
     case Map.lookup name graph of
         Nothing -> error $ "Invalid name " ++ name
         Just (Constant i) -> A.constant i
-        Just (BinOp op l r) -> op (computeNodeExp l graph) (computeNodeExp r graph)
+        Just (BinOp op l r) -> opToExp op (computeNodeExp l graph) (computeNodeExp r graph)
 
 computeNode :: String -> ComputeGraph -> Int
 computeNode name graph =
@@ -55,11 +74,35 @@ computeNode name graph =
         acc = A.unit exp
     in A.indexArray (AI.run acc) A.Z
 
+computeGraphToZ3Q2 :: ComputeGraph -> Z3 (Maybe Integer)
+computeGraphToZ3Q2 graph = do
+    let (BinOp _ eqL eqR) = graph ^?! ix "root"
+    humnNode <- mkFreshIntVar "humn"
+    let nodeToZ3 name =
+            case name of
+                "humn" -> return humnNode
+                _ -> case graph ^?! ix name of
+                    BinOp op l r -> do
+                        lAst <- nodeToZ3 l
+                        rAst <- nodeToZ3 r
+                        opToZ3 op lAst rAst
+                    Constant i -> mkInteger $ fromIntegral i
+    eqLast <- nodeToZ3 eqL
+    eqRast <- nodeToZ3 eqR
+    eqZ3 <- mkEq eqLast eqRast
+    Z3.assert eqZ3
+    fmap (join . snd) $ withModel $ \m -> evalInt m humnNode
+
 main :: IO ()
 main = do
     testGraph <- parseOrDie parseComputeGraph <$> TIO.readFile "aoc22_day21_test"
     let actualRootVal = computeNode "root" testGraph
     print actualRootVal
-    assert (actualRootVal == 152) $ return ()
+    -- assert (actualRootVal == 152) $ return ()
     graph <- parseOrDie parseComputeGraph <$> TIO.readFile "aoc22_day21_input"
     putStrLn $ "Question 1 answer is: " ++ show (computeNode "root" graph)
+    let graphScript = computeGraphToZ3Q2 graph
+    maybeSolution <- evalZ3 graphScript
+    case maybeSolution of
+        Nothing -> putStrLn $ "No solution found for Q2!"
+        Just sol -> putStrLn $ "Question 2 answer is: " ++ show sol
