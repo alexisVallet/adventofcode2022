b4da07b4431bbb9adad7d2c3a1cef74a61a30fd0
Alexis Vallet <alexisvallet@preferred.jp>
2022-12-30 22:09:59 +0900

    Formatting.

--- aoc2022_day_19_q1/ParetoFront.hs
+++ aoc2022_day_19_q1/ParetoFront.hs
@@ -1,4 +1,4 @@
-{- 
+{-
 Attempt at a somewhat efficient N-dimensional pareto front data structure.
 -}
 module ParetoFront where
@@ -8,40 +8,42 @@ import Imports
 
 type Element = [Int]
 
-newtype ParetoFront a = PF {
-    unPF :: Either (IntMap a) (IntMap (ParetoFront a))
-} deriving (Generic, Eq, Show)
+newtype ParetoFront a = PF
+  { unPF :: Either (IntMap a) (IntMap (ParetoFront a))
+  }
+  deriving (Generic, Eq, Show)
+
 instance (NFData a) => NFData (ParetoFront a)
 
 instance Functor ParetoFront where
-    fmap f (PF (Left leafMap)) = PF (Left (fmap f leafMap))
-    fmap f (PF (Right nodeMap)) = PF (Right (fmap (fmap f) nodeMap))
+  fmap f (PF (Left leafMap)) = PF (Left (fmap f leafMap))
+  fmap f (PF (Right nodeMap)) = PF (Right (fmap (fmap f) nodeMap))
 
 instance Foldable ParetoFront where
-    foldr f b (PF (Left leafMap)) = foldr f b leafMap
-    foldr f b (PF (Right nodeMap)) = foldr (flip (foldr f)) b nodeMap
+  foldr f b (PF (Left leafMap)) = foldr f b leafMap
+  foldr f b (PF (Right nodeMap)) = foldr (flip (foldr f)) b nodeMap
 
 foldrWithKey :: (Element -> a -> b -> b) -> b -> ParetoFront a -> b
-foldrWithKey f b (PF (Left leafMap)) = IMap.foldrWithKey (f . (:[])) b leafMap
-foldrWithKey f b (PF (Right nodeMap)) = IMap.foldrWithKey (\k a b' -> foldrWithKey (f . (k:)) b' a) b nodeMap
+foldrWithKey f b (PF (Left leafMap)) = IMap.foldrWithKey (f . (: [])) b leafMap
+foldrWithKey f b (PF (Right nodeMap)) = IMap.foldrWithKey (\k a b' -> foldrWithKey (f . (k :)) b' a) b nodeMap
 
 frontAssocs :: ParetoFront a -> [(Element, a)]
-frontAssocs = foldrWithKey (\el a curList -> (el, a):curList) []
+frontAssocs = foldrWithKey (\el a curList -> (el, a) : curList) []
 
 splitLevel :: Int -> IntMap a -> (IntMap a, IntMap a)
 splitLevel i imap =
-    let (lower, mEq, greater) = IMap.splitLookup i imap
-        tryInsert submap = case mEq of
-            Nothing -> submap
-            Just a -> IMap.insert i a submap
-    in (tryInsert lower, tryInsert greater)
+  let (lower, mEq, greater) = IMap.splitLookup i imap
+      tryInsert submap = case mEq of
+        Nothing -> submap
+        Just a -> IMap.insert i a submap
+   in (tryInsert lower, tryInsert greater)
 
 iMapUnzip :: IntMap (a, a) -> (IntMap a, IntMap a)
 iMapUnzip map =
-    let (keys, a1s, a2s) = unzip3 $ (\(k, (a1, a2)) -> (k, a1, a2)) <$> IMap.assocs map
-    in
-        (IMap.fromDistinctAscList $ zip keys a1s,
-        IMap.fromDistinctAscList $ zip keys a2s)
+  let (keys, a1s, a2s) = unzip3 $ (\(k, (a1, a2)) -> (k, a1, a2)) <$> IMap.assocs map
+   in ( IMap.fromDistinctAscList $ zip keys a1s,
+        IMap.fromDistinctAscList $ zip keys a2s
+      )
 
 -- Based on a (potentially new) element, splits a pareto front into
 -- a (dominated, dominant) elements in a divide and conquer
@@ -54,44 +56,45 @@ splitPareto [_] (PF (Right _)) = error "Element is not large enough!"
 -- At the leaf node level, we have a total order, so no elements are
 -- incomparable.
 splitPareto [i] (PF (Left leafMap)) = do
-    let (lowerEq, greaterEq) = splitLevel i leafMap
-     in (PF (Left lowerEq), PF (Left greaterEq))
-splitPareto (_:_) (PF (Left _)) = error "Element is too large!"
+  let (lowerEq, greaterEq) = splitLevel i leafMap
+   in (PF (Left lowerEq), PF (Left greaterEq))
+splitPareto (_ : _) (PF (Left _)) = error "Element is too large!"
 -- At the node level, we split and call recursively on both the lowerEq
 -- and greaterEq side. The lowerEq -> lowerEq case are dominated, the
 -- greaterEq -> greaterEq are dominant.
 -- The remaining elements are non comparable, so we ignore them.
-splitPareto (i:is) (PF (Right nodeMap)) =
-    let (lowerEq, greaterEq) = splitLevel i nodeMap
-        (lowerDominated, _) = iMapUnzip $ fmap (splitPareto is) lowerEq
-        (_, greaterDominant) = iMapUnzip $ fmap (splitPareto is) greaterEq
-     in (PF (Right lowerDominated), PF (Right greaterDominant))
+splitPareto (i : is) (PF (Right nodeMap)) =
+  let (lowerEq, greaterEq) = splitLevel i nodeMap
+      (lowerDominated, _) = iMapUnzip $ fmap (splitPareto is) lowerEq
+      (_, greaterDominant) = iMapUnzip $ fmap (splitPareto is) greaterEq
+   in (PF (Right lowerDominated), PF (Right greaterDominant))
 
 frontLookup :: Element -> ParetoFront a -> Maybe a
 frontLookup [i] (PF (Left leafMap)) = IMap.lookup i leafMap
-frontLookup (i:is) (PF (Right nodeMap)) = do
-    subMap <- IMap.lookup i nodeMap
-    frontLookup is subMap
+frontLookup (i : is) (PF (Right nodeMap)) = do
+  subMap <- IMap.lookup i nodeMap
+  frontLookup is subMap
 frontLookup _ _ = error "The element is either too large ot too small for the front!"
 
 -- O(n + m)
 frontDifference :: ParetoFront a -> ParetoFront a -> ParetoFront a
 frontDifference (PF (Left leafMap1)) (PF (Left leafMap2)) = PF (Left (IMap.difference leafMap1 leafMap2))
 frontDifference (PF (Right nodeMap1)) (PF (Right nodeMap2)) =
-    PF (Right $ IMap.differenceWithKey recurseDiff nodeMap1 nodeMap2)
-    where recurseDiff _ m1 m2 = 
-            let diffOut = frontDifference m1 m2
-             in if null diffOut then Nothing else Just diffOut
+  PF (Right $ IMap.differenceWithKey recurseDiff nodeMap1 nodeMap2)
+  where
+    recurseDiff _ m1 m2 =
+      let diffOut = frontDifference m1 m2
+       in if null diffOut then Nothing else Just diffOut
 frontDifference _ _ = error "fronts do not have the same depth!"
 
 frontInsert :: Element -> a -> ParetoFront a -> ParetoFront a
 frontInsert [i] a (PF (Left leafMap)) = PF (Left $ IMap.insert i a leafMap)
-frontInsert (i:is) a (PF (Right nodeMap)) = PF (Right $ IMap.alter (Just . maybe (frontSingleton is a) (frontInsert is a)) i nodeMap)
+frontInsert (i : is) a (PF (Right nodeMap)) = PF (Right $ IMap.alter (Just . maybe (frontSingleton is a) (frontInsert is a)) i nodeMap)
 frontInsert _ _ _ = error "The element is either too large or too small for the front!"
 
 frontSingleton :: Element -> a -> ParetoFront a
 frontSingleton [i] a = PF (Left $ IMap.singleton i a)
-frontSingleton (i:is) a = PF (Right $ IMap.singleton i (frontSingleton is a))
+frontSingleton (i : is) a = PF (Right $ IMap.singleton i (frontSingleton is a))
 frontSingleton _ _ = error "Element cannot be an empty list!"
 
 -- Empty pareto front has no defined depth just yet.
@@ -105,29 +108,34 @@ frontEmpty = PF (Right mempty)
 -- O(d log(n)^2) if the element is dominated by any element in the front, or non comparable to all.
 -- O(n) if the element is dominating any element in the front.
 -- Hopefully this works out alright for our use cases.
--- 
+--
 -- Returns (mVal, newFront, isDominated) where:
+
 -- * mVal is the result of looking up the element. Nothing if the element is new,
+
 --   or yet to be computed. Just x if the value has been computed.
+
 -- * newFront is the front updated with the new value if the new value is not dominated.
+
 -- * isDominated is true if the value is dominated by any value in the pareto front.
+
 insertLookup :: Element -> ParetoFront (Maybe a) -> (Maybe a, ParetoFront (Maybe a), Bool)
 insertLookup elem front =
-    let lookupRes = join $ frontLookup elem front
-        (newFront, isDominated) = case lookupRes of
-            Just _ -> (front, False)
-            Nothing ->
-                let (dominated, dominating) = splitPareto elem front
-                in if not (null dominating) then
-                    -- if elem is dominated by any element, return the pareto front
-                    -- unchanged as it should not be inserted.
-                    (front, True)
-                   else if not (null dominated) then
-                    -- if elem is dominating any element. We remove all dominated
+  let lookupRes = join $ frontLookup elem front
+      (newFront, isDominated) = case lookupRes of
+        Just _ -> (front, False)
+        Nothing ->
+          let (dominated, dominating) = splitPareto elem front
+           in if not (null dominating)
+                then -- if elem is dominated by any element, return the pareto front
+                -- unchanged as it should not be inserted.
+                  (front, True)
+                else
+                  if not (null dominated)
+                    then -- if elem is dominating any element. We remove all dominated
                     -- elements, and insert the new element in the front.
-                    (frontInsert elem Nothing $ frontDifference front dominated, False)
-                   else
-                    -- Otherwise, elem is not comparable to existing elements in the
+                      (frontInsert elem Nothing $ frontDifference front dominated, False)
+                    else -- Otherwise, elem is not comparable to existing elements in the
                     -- pareto front, so we simply add it.
-                    (frontInsert elem Nothing front, False)
-     in (lookupRes, newFront, isDominated)
+                      (frontInsert elem Nothing front, False)
+   in (lookupRes, newFront, isDominated)
